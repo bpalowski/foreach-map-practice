4281349db7db5570b39d334d641ff51c789986e3
Brian Palowski and James Hanley <brian@>
2018-06-04 15:45:50 -0700

    Create ouline for array push

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
   var itemCount = 0;
+  var returnList = [];
   $('#addItem').click(function(event){
-    debugger;
     event.preventDefault();
     itemCount += 1;
     $('ul#itemList').prepend('<li><input type="text" class="form-control" id="item' +itemCount+'"></li>');
   });
+  $('#submitList').click(function(event){
+    event.preventDefault();
+//    itemCount.forEach(item(function(){
+//      var userInput = $("input#item" + item).val();
+//      returnList.push(userInput);
+    alert(itemCount);
+   });
+    console.log(returnList);
+
 
 });
